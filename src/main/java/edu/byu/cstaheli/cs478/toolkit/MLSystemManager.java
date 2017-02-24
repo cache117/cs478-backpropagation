c02798aa59f10ebea839f1be27226d682fe59fa2
Cache Staheli <cache@byu.edu>
2017-02-23 22:22:39 -0700

    Tweak Manager's ML loading.
    The specific ML algorithm can be loaded without using the command
    line. This can be especially helpful for testing, when you need to
    set all of the weights to specific values to get deterministic
    results that can be tested and verified to be the same, every time.
    This will allow for building the layers independently of the
    command line, which will be helpful for the lab itself.

--- src/main/java/edu/byu/cstaheli/cs478/toolkit/MLSystemManager.java
+++ src/main/java/edu/byu/cstaheli/cs478/toolkit/MLSystemManager.java
@@ -7,7 +7,6 @@ package edu.byu.cstaheli.cs478.toolkit;
 
 import edu.byu.cstaheli.cs478.backpropogation.BackPropagation;
 import edu.byu.cstaheli.cs478.baseline.BaselineLearner;
-import edu.byu.cstaheli.cs478.perceptron.MultipleOutputPerceptron;
 import edu.byu.cstaheli.cs478.perceptron.Perceptron;
 import edu.byu.cstaheli.cs478.toolkit.strategy.*;
 
@@ -59,6 +58,11 @@ public class MLSystemManager
         return learner;
     }
 
+    public void setLearner(SupervisedLearner learner)
+    {
+        this.learner = learner;
+    }
+
     public void run(String[] args) throws Exception
     {
 
@@ -78,13 +82,9 @@ public class MLSystemManager
     private void determineEvalMethod(ArgParser parser) throws Exception
     {
         // Load the model
-        if (parser.getWantedIndex() == null)
+        if (learner == null)
         {
             learner = getLearner(parser.getLearner(), getRandom());
-        }
-        else
-        {
-            learner = new MultipleOutputPerceptron(getRandom(), this, Double.parseDouble(parser.getWantedIndex()));
         }
 
         // Load the ARFF file
@@ -141,16 +141,10 @@ public class MLSystemManager
     private void calcStatic(SupervisedLearner learner, LearnerData learnerData) throws Exception
     {
         LearningStrategy strategy = new StaticStrategy(learnerData);
-//        Matrix testData = new Matrix();
-//        testData.loadArff(learnerData.getEvalParameter());
-//        if (learnerData.isNormalized())
-//            testData.normalize(); // BUG! This may normalize differently from the training data. It should use the same ranges for normalization!
 
         System.out.println("Calculating accuracy on separate test set...");
         System.out.println("Test set name: " + learnerData.getEvalParameter());
         System.out.println("Number of test instances: " + strategy.getTestingData().rows());
-//        Matrix features = new Matrix(learnerData.getArffData(), 0, 0, learnerData.getArffData().rows(), learnerData.getArffData().cols() - 1);
-//        Matrix labels = new Matrix(learnerData.getArffData(), 0, learnerData.getArffData().cols() - 1, learnerData.getArffData().rows(), 1);
         double startTime = System.currentTimeMillis();
         learner.train(strategy);
         double elapsedTime = System.currentTimeMillis() - startTime;
@@ -159,8 +153,6 @@ public class MLSystemManager
         System.out.println("Training set accuracy: " + trainAccuracy);
         Matrix testFeatures = strategy.getTestingFeatures();
         Matrix testLabels = strategy.getTestingLabels();
-//        Matrix testFeatures = new Matrix(testData, 0, 0, testData.rows(), testData.cols() - 1);
-//        Matrix testLabels = new Matrix(testData, 0, testData.cols() - 1, testData.rows(), 1);
         Matrix confusion = new Matrix();
         double testAccuracy = learner.measureAccuracy(testFeatures, testLabels, confusion);
         System.out.println("Test set accuracy: " + testAccuracy);
@@ -183,11 +175,6 @@ public class MLSystemManager
         System.out.println("Percentage used for training: " + trainPercent);
         System.out.println("Percentage used for testing: " + (1 - trainPercent));
         learnerData.getArffData().shuffle(learnerData.getRandom());
-//        int trainSize = (int) (trainPercent * data.rows());
-//        Matrix trainFeatures = new Matrix(data, 0, 0, trainSize, data.cols() - 1);
-//        Matrix trainLabels = new Matrix(data, 0, data.cols() - 1, trainSize, 1);
-//        Matrix testFeatures = new Matrix(data, trainSize, 0, data.rows() - trainSize, data.cols() - 1);
-//        Matrix testLabels = new Matrix(data, trainSize, data.cols() - 1, data.rows() - trainSize, 1);
         Matrix testFeatures = strategy.getTestingFeatures();
         Matrix testLabels = strategy.getTestingLabels();
         double startTime = System.currentTimeMillis();
@@ -228,20 +215,21 @@ public class MLSystemManager
                 int begin = i * learnerData.getArffData().rows() / folds;
                 int end = (i + 1) * learnerData.getArffData().rows() / folds;
                 strategy = new CrossValidationStrategy(learnerData, begin, end);
-//                Matrix trainFeatures = new Matrix(data, 0, 0, begin, data.cols() - 1);
-//                Matrix trainLabels = new Matrix(data, 0, data.cols() - 1, begin, 1);
-//                Matrix testFeatures = new Matrix(data, begin, 0, end - begin, data.cols() - 1);
-//                Matrix testLabels = new Matrix(data, begin, data.cols() - 1, end - begin, 1);
                 Matrix testFeatures = strategy.getTestingFeatures();
                 Matrix testLabels = strategy.getTestingLabels();
-//                trainFeatures.add(data, end, 0, data.rows() - end);
-//                trainLabels.add(data, end, data.cols() - 1, data.rows() - end);
                 double startTime = System.currentTimeMillis();
                 learner.train(strategy);
                 elapsedTime += System.currentTimeMillis() - startTime;
-                double accuracy = learner.measureAccuracy(testFeatures, testLabels, null);
+                Matrix confusion = new Matrix();
+                double accuracy = learner.measureAccuracy(testFeatures, testLabels, confusion);
                 sumAccuracy += accuracy;
                 System.out.println("Rep=" + j + ", Fold=" + i + ", Accuracy=" + accuracy);
+                if (learnerData.isVerbose())
+                {
+                    System.out.println("\nConfusion matrix: (Row=target value, Col=predicted value)");
+                    confusion.print();
+                    System.out.println("\n");
+                }
             }
         }
         elapsedTime /= (reps * folds);
